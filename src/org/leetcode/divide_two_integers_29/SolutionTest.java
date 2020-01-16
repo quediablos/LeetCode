0299b0e9fb777f5f4907e58ad0c19ad99e5ea88a
Sacit Bayraktar <sacit.bayraktar@amadeus.com>
2020-01-16 16:23:37 +0300

    divide_two_integers_29: implemented overflow cases.

--- src/org/leetcode/divide_two_integers_29/SolutionTest.java
+++ src/org/leetcode/divide_two_integers_29/SolutionTest.java
@@ -30,6 +30,34 @@ public class SolutionTest {
         assertTrue(solution.divide(1000,-2) == -500);
     }
 
+    @Test
+    public void test_Custom_4()
+    {
+        Solution solution = new Solution();
+        assertTrue(solution.divide(1001,2) == 500);
+    }
+
+    @Test
+    public void test_Custom_5()
+    {
+        Solution solution = new Solution();
+        assertTrue(solution.divide(10011,2) == 5005);
+    }
+
+    @Test
+    public void test_Custom_6()
+    {
+        Solution solution = new Solution();
+        assertTrue(solution.divide(1_000,2) == 500);
+    }
+
+    @Test
+    public void test_Custom()
+    {
+        Solution solution = new Solution();
+        assertTrue(solution.divide(2147483647,-1) == -2147483647);
+    }
+
     /************************* Leet code cases **********************/
 
     @Test
@@ -45,5 +73,50 @@ public class SolutionTest {
         Solution solution = new Solution();
         assertTrue(solution.divide(7,-3) == -2);
     }
+
+    @Test
+    public void test_5()
+    {
+        Solution solution = new Solution();
+        assertTrue(solution.divide(-1,1) == -1);
+    }
+
+    @Test
+    public void test_17()
+    {
+
+        Solution solution = new Solution();
+        assertTrue(solution.divide(-2147483648,-1) == 2147483647);
+    }
+
+    @Test
+    public void test_18()
+    {
+        Solution solution = new Solution();
+        assertTrue(solution.divide(2_147_483_647,2) == 1_073_741_823);
+    }
+
+    @Test
+    public void test_19()
+    {
+        Solution solution = new Solution();
+        assertTrue(solution.divide(-2_147_483_648,2) == -1_073_741_824);
+    }
+
+    @Test
+    public void test_21()
+    {
+
+        Solution solution = new Solution();
+        assertTrue(solution.divide(-1_010_369_383,-2_147_483_648) == 0);
+    }
+
+    @Test
+    public void test_571()
+    {
+        Solution solution = new Solution();
+        assertTrue(solution.divide(-1_060_849_722,99_958_928) == -10);
+    }
+
 
 }
