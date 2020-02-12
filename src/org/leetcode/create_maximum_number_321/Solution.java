c728556dfc0f06d11def6a028868dfb5c470246c
quediablos <msacitbayraktar@gmail.com>
2020-02-12 13:31:07 +0300

    create_maximum_number_321: started

--- src/org/leetcode/create_maximum_number_321/Solution.java
+++ src/org/leetcode/create_maximum_number_321/Solution.java
@@ -1,87 +1,68 @@
 package org.leetcode.create_maximum_number_321;
 
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Queue;
 
 public class Solution
 {
+    private static final int MAX_DIGIT = 9;
     public class Value
     {
         public int number;
         public int array;
-        public int index;
+        public int index1;
+        public int index2;
+        public Value from;
+        public int degree;
 
-        public Value(int number, int array, int index)
-        {
+        public Value(int number, int array, int index1, int index2, Value from, int degree) {
             this.number = number;
             this.array = array;
-            this.index = index;
+            this.index1 = index1;
+            this.index2 = index2;
+            this.from = from;
+            this.degree = degree;
         }
 
         @Override
-        public String toString()
-        {
+        public String toString() {
             return "Value{" +
                     "number=" + number +
                     ", array=" + array +
-                    ", index=" + index +
+                    ", index1=" + index1 +
+                    ", index2=" + index2 +
+                    ", from=" + from +
+                    ", degree=" + degree +
                     '}';
         }
     }
 
-    public class Node
+    public class MutableInt
     {
-        public Value value;
-        public Node right;
-        public Node left;
+        public int val;
 
-        public Node(Value value)
+        public MutableInt(int val)
         {
-            this.value = value;
+            this.val = val;
         }
     }
 
-    public Node insertToTree(Node root,Value value)
-    {
-        if (root == null)
-            return new Node(value);
-        else if (value.number >= root.value.number)
-            root.right = insertToTree(root.right,value);
-        else
-            root.left = insertToTree(root.left,value);
 
-        return root;
+
+    private int[] makeNumber(Value value,int length)
+    {
+        Value v = value;
+        int[] number = new int[length];
+
+        for (int i=length-1; i>=0; i--)
+        {
+            number[i] = v.number;
+            v = v.from;
+        }
+
+        return number;
     }
 
-    private List<Value> sortArrays(int[] nums1, int[] nums2)
-    {
-        Node root = null;
-
-        for (int i=0; i<nums1.length; i++)
-            root = insertToTree(root,new Value(nums1[i],1,i));
-
-        for (int i=0; i<nums2.length; i++)
-            root = insertToTree(root,new Value(nums2[i],2,i));
-
-        List<Value> sortedList = new LinkedList<>();
-
-        treeToArray(root,sortedList);
-
-        return sortedList;
-
-    }
-
-    private void treeToArray(Node root,List<Value> list)
-    {
-        if (root == null)
-            return;
-
-        treeToArray(root.right,list);
-
-        list.add(root.value);
-
-        treeToArray(root.left,list);
-    }
 
     /**
      * Interface method
@@ -94,49 +75,19 @@ public class Solution
     {
         //TODO:edge cases
 
-        List<Value> sortedList = sortArrays(nums1,nums2);
-
         int index1 = -1;
         int index2 = -1;
-        int[] resultArray = new int[k];
-        int resultArrayInd = 0;
-        int remainingNumbersInResult = k;
+        int remainingNumbers = k;
+        int[] result = new int[k];
+        int resultInd = 0;
+        MutableInt allowedSkips = new MutableInt(nums1.length + nums2.length - k);
 
-        for (Value value : sortedList)
+        while (remainingNumbers > 0)
         {
-            int remainingNumbersAtArray1,remainingNumbersAtArray2;
 
-            if (remainingNumbersInResult == 0)
-                break;
-
-            //taking from array 1
-            if (value.array == 1 && index1 < value.index)
-            {
-                remainingNumbersAtArray1 = nums1.length - value.index - 1;
-                remainingNumbersAtArray2 = nums2.length - index1 - 1;
-
-                if (remainingNumbersAtArray1 + remainingNumbersAtArray2 >= remainingNumbersInResult-1)
-                {
-                    resultArray[resultArrayInd++] = value.number;
-                    index1 = value.index;
-                    remainingNumbersInResult--;
-                }
-            }
-            //taking from array 2
-            else if (value.array == 2 && index2 < value.index)
-            {
-                remainingNumbersAtArray1 = nums1.length - index1 - 1;
-                remainingNumbersAtArray2 = nums2.length - value.index - 1;
-
-                if (remainingNumbersAtArray1 + remainingNumbersAtArray2 >= remainingNumbersInResult-1)
-                {
-                    resultArray[resultArrayInd++] = value.number;
-                    index2 = value.index;
-                    remainingNumbersInResult--;
-                }
-            }
         }
 
-        return resultArray;
+        return result;
+
     }
 }
--- src/org/leetcode/create_maximum_number_321/SolutionTest.java
+++ src/org/leetcode/create_maximum_number_321/SolutionTest.java
@@ -7,6 +7,53 @@ public class SolutionTest
 {
     /*********************** Own cases ************************/
 
+    @Test
+    public void testOwn_1()
+    {
+        Solution solution = new Solution();
+        int[] expectedResult = new int[] {8,9,1,2,3,4,5,6,7};
+        int[] nums1 = new int[] {1,2,3,4,5,6,7};
+        int[] nums2 = new int[] {8,9};
+        int k = 9;
+
+        Assert.assertArrayEquals(expectedResult,solution.maxNumber(nums1,nums2,k));
+    }
+
+    @Test
+    public void testOwn_3()
+    {
+        Solution solution = new Solution();
+        int[] expectedResult = new int[] {9,9,7,7};
+        int[] nums1 = new int[] {8,9,7,3};
+        int[] nums2 = new int[] {8,9,7,3};
+        int k = 4;
+
+        Assert.assertArrayEquals(expectedResult,solution.maxNumber(nums1,nums2,k));
+    }
+
+    @Test
+    public void testOwn_2()
+    {
+        Solution solution = new Solution();
+        int[] expectedResult = new int[] {8,7};
+        int[] nums1 = new int[] {7};
+        int[] nums2 = new int[] {8};
+        int k = 2;
+
+        Assert.assertArrayEquals(expectedResult,solution.maxNumber(nums1,nums2,k));
+    }
+
+    @Test
+    public void testOwn_4()
+    {
+        Solution solution = new Solution();
+        int[] expectedResult = new int[] {0,1,7,0,1,6};
+        int[] nums1 = new int[] {0,1,7};
+        int[] nums2 = new int[] {0,1,6};
+        int k = 6;
+
+        Assert.assertArrayEquals(expectedResult,solution.maxNumber(nums1,nums2,k));
+    }
 
     /************************ LC cases *************************/
 
@@ -33,4 +80,84 @@ public class SolutionTest
 
         Assert.assertArrayEquals(expectedResult,solution.maxNumber(nums1,nums2,k));
     }
+
+
+    @Test
+    public void testLC_3()
+    {
+        Solution solution = new Solution();
+        int[] expectedResult = new int[] {9,8,9};
+        int[] nums1 = new int[] {3,9};
+        int[] nums2 = new int[] {8,9};
+        int k = 3;
+
+        Assert.assertArrayEquals(expectedResult,solution.maxNumber(nums1,nums2,k));
+    }
+
+    @Test
+    public void testLC_89()
+    {
+        Solution solution = new Solution();
+        int[] expectedResult = new int[] {9,8,9};
+        int[] nums1 = new int[] {8,9,7,3,5,9,1,0,8,5,3,0,9,2,7,4,8,9,8,1,0,2,0,2,7,2,3,5,4,7,4,1,4,0,1,4,2,1,3,1,5,3,9,3,9,0,1,7,0,6,1,8,5,6,6,5,0,4,7,2,9,2,2,7,6,2,9,2,3,5,7,4,7,0,1,8,3,6,6,3,0,8,5,3,0,3,7,3,0,9,8,5,1,9,5,0,7,9,6,8,5,1,9,6,5,8,2,3,7,1,0,1,4,3,4,4,2,4,0,8,4,6,5,5,7,6,9,0,8,4,6,1,6,7,2,0,1,1,8,2,6,4,0,5,5,2,6,1,6,4,7,1,7,2,2,9,8,9,1,0,5,5,9,7,7,8,8,3,3,8,9,3,7,5,3,6,1,0,1,0,9,3,7,8,4,0,3,5,8,1,0,5,7,2,8,4,9,5,6,8,1,1,8,7,3,2,3,4,8,7,9,9,7,8,5,2,2,7,1,9,1,5,5,1,3,5,9,0,5,2,9,4,2,8,7,3,9,4,7,4,8,7,5,0,9,9,7,9,3,8,0,9,5,3,0,0,3,0,4,9,0,9,1,6,0,2,0,5,2,2,6,0,0,9,6,3,4,1,2,0,8,3,6,6,9,0,2,1,6,9,2,4,9,0,8,3,9,0,5,4,5,4,6,1,2,5,2,2,1,7,3,8,1,1,6,8,8,1,8,5,6,1,3,0,1,3,5,6,5,0,6,4,2,8,6,0,3,7,9,5,5,9,8,0,4,8,6,0,8,6,6,1,6,2,7,1,0,2,2,4,0,0,0,4,6,5,5,4,0,1,5,8,3,2,0,9,7,6,2,6,9,9,9,7,1,4,6,2,8,2,5,3,4,5,2,4,4,4,7,2,2,5,3,2,8,2,2,4,9,8,0,9,8,7,6,2,6,7,5,4,7,5,1,0,5,7,8,7,7,8,9,7,0,3,7,7,4,7,2,0,4,1,1,9,1,7,5,0,5,6,6,1,0,6,9,4,2,8,0,5,1,9,8,4,0,3,1,2,4,2,1,8,9,5,9,6,5,3,1,8,9,0,9,8,3,0,9,4,1,1,6,0,5,9,0,8,3,7,8,5};
+        int[] nums2 = new int[] {8,9,7,3,5,9,1,0,8,5,3,0,9,2,7,4,8,9,8,1,0,2,0,2,7,2,3,5,4,7,4,1,4,0,1,4,2,1,3,1,5,3,9,3,9,0,1,7,0,6,1,8,5,6,6,5,0,4,7,2,9,2,2,7,6,2,9,2,3,5,7,4,7,0,1,8,3,6,6,3,0,8,5,3,0,3,7,3,0,9,8,5,1,9,5,0,7,9,6,8,5,1,9,6,5,8,2,3,7,1,0,1,4,3,4,4,2,4,0,8,4,6,5,5,7,6,9,0,8,4,6,1,6,7,2,0,1,1,8,2,6,4,0,5,5,2,6,1,6,4,7,1,7,2,2,9,8,9,1,0,5,5,9,7,7,8,8,3,3,8,9,3,7,5,3,6,1,0,1,0,9,3,7,8,4,0,3,5,8,1,0,5,7,2,8,4,9,5,6,8,1,1,8,7,3,2,3,4,8,7,9,9,7,8,5,2,2,7,1,9,1,5,5,1,3,5,9,0,5,2,9,4,2,8,7,3,9,4,7,4,8,7,5,0,9,9,7,9,3,8,0,9,5,3,0,0,3,0,4,9,0,9,1,6,0,2,0,5,2,2,6,0,0,9,6,3,4,1,2,0,8,3,6,6,9,0,2,1,6,9,2,4,9,0,8,3,9,0,5,4,5,4,6,1,2,5,2,2,1,7,3,8,1,1,6,8,8,1,8,5,6,1,3,0,1,3,5,6,5,0,6,4,2,8,6,0,3,7,9,5,5,9,8,0,4,8,6,0,8,6,6,1,6,2,7,1,0,2,2,4,0,0,0,4,6,5,5,4,0,1,5,8,3,2,0,9,7,6,2,6,9,9,9,7,1,4,6,2,8,2,5,3,4,5,2,4,4,4,7,2,2,5,3,2,8,2,2,4,9,8,0,9,8,7,6,2,6,7,5,4,7,5,1,0,5,7,8,7,7,8,9,7,0,3,7,7,4,7,2,0,4,1,1,9,1,7,5,0,5,6,6,1,0,6,9,4,2,8,0,5,1,9,8,4,0,3,1,2,4,2,1,8,9,5,9,6,5,3,1,8,9,0,9,8,3,0,9,4,1,1,6,0,5,9,0,8,3,7,8,5};
+        int k = 500;
+
+        Assert.assertArrayEquals(expectedResult,solution.maxNumber(nums1,nums2,k));
+    }
+
+    /**
+     * [8,9]
+     * [3,9]
+     * 3
+     */
+    @Test
+    public void testLC_64()
+    {
+        Solution solution = new Solution();
+        int[] expectedResult = new int[] {9,8,9};
+        int[] nums1 = new int[] {8,9};
+        int[] nums2 = new int[] {3,9};
+        int k = 3;
+
+        Assert.assertArrayEquals(expectedResult,solution.maxNumber(nums1,nums2,k));
+    }
+
+    @Test
+    public void testLC_59()
+    {
+        Solution solution = new Solution();
+        int[] expectedResult = new int[] {8,8,8,5,4 };
+        int[] nums1 = new int[] {3,8,5,3,4};
+        int[] nums2 = new int[] {8,7,3,6,8};
+        int k = 5;
+
+        Assert.assertArrayEquals(expectedResult,solution.maxNumber(nums1,nums2,k));
+    }
+
+
+    @Test
+    public void testLC_65()
+    {
+        Solution solution = new Solution();
+        int[] expectedResult = new int[] {7,3,8,2,5,6,4,4,0,6,5,7,6,2,0};
+        int[] nums1 = new int[] {2,5,6,4,4,0};
+        int[] nums2 = new int[] {7,3,8,0,6,5,7,6,2};
+        int k = 15;
+
+        Assert.assertArrayEquals(expectedResult,solution.maxNumber(nums1,nums2,k));
+    }
+
+    @Test
+    public void testLC_80()
+    {
+        Solution solution = new Solution();
+        int[] expectedResult = new int[] {2,6,2,0,1,0,5,4,5,5,3,3,3,4};
+        int[] nums1 = new int[] {2,1,7,8,0,1,7,3,5,8,9,0,0,7,0,2,2,7,3,5,5};
+        int[] nums2 = new int[] {2,6,2,0,1,0,5,4,5,5,3,3,3,4};
+        int k = 35;
+
+        Assert.assertArrayEquals(expectedResult,solution.maxNumber(nums1,nums2,k));
+    }
+
 }
