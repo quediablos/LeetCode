5657ac956ec893774b2c8f1a419aede535e95546
quediablos <msacitbayraktar@gmail.com>
2020-02-14 15:56:55 +0300

    create maximum number: working

--- src/org/leetcode/create_maximum_number_321/SolutionTest.java
+++ src/org/leetcode/create_maximum_number_321/SolutionTest.java
@@ -152,10 +152,23 @@ public class SolutionTest
     public void testLC_80()
     {
         Solution solution = new Solution();
-        int[] expectedResult = new int[] {2,6,2,0,1,0,5,4,5,5,3,3,3,4};
+        int[] expectedResult = new int[] {2,6,2,2,1,7,8,0,1,7,3,5,8,9,0,1,0,5,4,5,5,3,3,3,4,0,0,7,0,2,2,7,3,5,5};
         int[] nums1 = new int[] {2,1,7,8,0,1,7,3,5,8,9,0,0,7,0,2,2,7,3,5,5};
         int[] nums2 = new int[] {2,6,2,0,1,0,5,4,5,5,3,3,3,4};
         int k = 35;
+
+        Assert.assertArrayEquals(expectedResult,solution.maxNumber(nums1,nums2,k));
+    }
+
+
+    @Test
+    public void testLC_99()
+    {
+        Solution solution = new Solution();
+        int[] expectedResult = new int[] {9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,6,8,8,4,4,5,7,5,2,8,2,7,7,7,4,8,5,0,9,6,9,2,0,2,4,2,2,1,6,6,5,3,6,2,9,6,4,5,9,7,8,0,7,2,3};
+        int[] nums1 = new int[]          {4,6,9,1,0,6,3,1,5,2,8,3,8,8,4,7,2,0,7,1,9,9,0,1,5,9,3,9,3,9,7,3,0,8,1,0,9,1,6,8,8,4,4,5,7,5,2,8,2,7,7,7,4,8,5,0,9,6,9,2};
+        int[] nums2 = new int[]          {9,9,4,5,1,2,0,9,3,4,6,3,0,9,2,8,8,2,4,8,6,5,4,4,2,9,5,0,7,3,7,5,9,6,6,8,8,0,2,4,2,2,1,6,6,5,3,6,2,9,6,4,5,9,7,8,0,7,2,3};
+        int k = 60;
 
         Assert.assertArrayEquals(expectedResult,solution.maxNumber(nums1,nums2,k));
     }
